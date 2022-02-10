887e1fc65861c764582d57c5b467e9f2bbae7c78
BAGU5 <35269946+BAGU5@users.noreply.github.com>
2022-02-10 18:43:42 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 git submodule update --recursive --init && ./scripts/applyPatches.shhttps://github.com/BAGU5/FlameCord/blob/master/scripts/build.sh
 if [ "$1" == "--jar" ]; then
     pushd FlameCord-Proxy
