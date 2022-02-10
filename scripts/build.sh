e67f73a87ad9637f418eb909123399331aa89825
BAGU5 <35269946+BAGU5@users.noreply.github.com>
2022-02-10 18:24:51 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-git submodule update --recursive --init && ./scripts/applyPatches.sh
+git submodule update --recursive --init && ./scripts/applyPatches.shhttps://github.com/BAGU5/FlameCord/blob/master/scripts/build.sh
 if [ "$1" == "--jar" ]; then
     pushd FlameCord-Proxy
     mvn clean package
